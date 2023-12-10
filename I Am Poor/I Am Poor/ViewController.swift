c78a9c5ecf9bab5e5732ab031a7ae64db26882e0
adrianrahn <44745066+adrianrahn@users.noreply.github.com>
2023-12-10 17:10:49 +0000

    Add files via upload

---
+++ I Am Poor/I Am Poor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Adrian Rahn on 09/07/2020.
+//  Copyright © 2020 Adrian Rahn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
